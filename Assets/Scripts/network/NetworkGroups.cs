0e87bd78f09f9766b4a5f90f56f73abf8aef36e2
Rob Briggs <superbriggs@googlemail.com>
2013-03-26 16:47:59 +0000

    Astraction in networkgroups (woot!)

---
+++ Assets/Scripts/network/NetworkGroups.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using System.Collections;
+
+public class NetworkGroups {
+
+	public static int[] Character = {0,1,2,3,4};
+	public static int[] Universe = {100,101,102,103,104};
+	public static int Everyone = 99;
+	public static int Bullets = 200;
+}
