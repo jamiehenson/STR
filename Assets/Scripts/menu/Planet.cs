6d210e8720eabc18d8fb0de776b14d3441619696
Rob Briggs <superbriggs@googlemail.com>
2013-04-12 01:12:11 +0100

    Revert "Revised background system, part 1"
    This reverts commit cc4a3500cfaf8f3b0f47b89dc62ebd7c9926b496.

--- Assets/Scripts/menu/Planet.cs
+++ Assets/Scripts/menu/Planet.cs
@@ -2,16 +2,9 @@ using UnityEngine;
 using System.Collections;
 
 public class Planet : MonoBehaviour {
-	
-	public float rotSpeed;
-	private float rotSimSpeed;
-	
-	void Start () {
-		rotSimSpeed = (Random.Range (0,0.8f));
-	}
-	
+
 	void Update () {
-		transform.Rotate(Vector3.up * Time.deltaTime * rotSimSpeed);
+		transform.Rotate(Vector3.up * Time.deltaTime * 0.3f);
 	}
 	
 }
