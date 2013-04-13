70ff8d650367cd477c23cb6512fcf99f855cb687
Jamie Henson <jhenson47@gmail.com>
2013-04-14 00:16:36 +0100

    BG panel flows better

--- Assets/Scripts/background/FlowingBGBehind2.cs
+++ Assets/Scripts/background/FlowingBGBehind2.cs
@@ -1,17 +1,17 @@
 using UnityEngine;
 using System.Collections;
 
-public class FlowingBGBehind : MonoBehaviour {
+public class FlowingBGBehind2 : MonoBehaviour {
 
 	public Transform BG;
 	private bool notcopied = true;
 	
-	private Object[] spacePix, objects;
+	private Texture2D spacePix;
 	
 	void Start () 
 	{
-		spacePix = Resources.LoadAll("bg/space", typeof(Texture2D));
-		gameObject.renderer.material.mainTexture = (Texture2D) spacePix[Random.Range (0,spacePix.Length)];
+		spacePix = (Texture2D) Resources.Load("bg/space/spacebg1");
+		gameObject.renderer.material.mainTexture = spacePix;
 	}
 	
 	// Update is called once per frame
@@ -19,7 +19,6 @@ public class FlowingBGBehind : MonoBehaviour {
 		transform.Translate(Vector3.right * Time.deltaTime * 3f);
 		if (transform.position.x < 0 && notcopied) {
 			Transform newBG = (Transform) Instantiate (BG, new Vector3(940,0,200), new Quaternion(0,0,0,0));
-			newBG.renderer.material.mainTexture = (Texture2D) spacePix[Random.Range (0,spacePix.Length)];
 			newBG.name = "BG slice";
 			newBG.transform.Rotate(new Vector3(90, 180, 0));
 			notcopied = false;
