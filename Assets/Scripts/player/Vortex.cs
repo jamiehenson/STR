30be379a26065ca60868006a120be67270413f28
Rob Briggs <superbriggs@googlemail.com>
2013-04-18 14:51:52 +0100

    Tried and failed to add label to vortex

--- Assets/Scripts/player/Vortex.cs
+++ Assets/Scripts/player/Vortex.cs
@@ -6,10 +6,11 @@ public class Vortex : MonoBehaviour {
     private int i;
 	public int leadsToUniverse;
     private float growth = 0.015f;
-	public int screenPositionX;
-	public int screenPositionY;
+	public float screenPositionX;
+	public float screenPositionY;
 	public string label;
 	private bool labelIsSet = false;
+	private Font deco = (Font) Resources.Load ("Belgrad");
 
     public static IEnumerator grow(GameObject vortex)
     {
@@ -35,11 +36,29 @@ public class Vortex : MonoBehaviour {
         Destroy(vortex);
     }
 
-	public void setLabel(int x, int y, int lab) {
+	public void setLabel(float x, float y, string lab) {
 		screenPositionX = x;
 		screenPositionY = y;
 		label = lab;
 		labelIsSet = true;
+	}
+
+	public void OnGUI() {
+
+		if (labelIsSet) {
+			Vector3 viewPort = new Vector3(screenPositionX,screenPositionY,0);
+			Vector3 screenPoint = Camera.main.ViewportToScreenPoint(viewPort);
+			print ("screenPoint = "+screenPoint);
+			int x = 10;
+			int y = 5;
+			GUIStyle style = new GUIStyle();
+	    	style.font = deco;
+			style.normal.textColor = Color.white;
+			style.fontStyle = FontStyle.Bold;
+			style.fontSize = 20;
+			style.normal.background = HudOn.fillTex(1,1,Color.black);
+			GUI.Label(new Rect(screenPoint.x,screenPoint.y,x,y), label, style);
+		}
 	}
 
     void Start()
@@ -64,6 +83,7 @@ public class Vortex : MonoBehaviour {
             transform.localScale += new Vector3(-growth, 0, -growth);
             i--;
         }
+
 	}
 
     void OnCollisionEnter(Collision collision)
