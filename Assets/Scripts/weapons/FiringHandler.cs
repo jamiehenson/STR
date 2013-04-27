d14a0ce93fad309aeb7e6979540897829bc88b71
Spektor0307 <madalinapatrichi@yahoo.com>
2013-04-27 02:58:42 +0100

    Mobile arm

--- Assets/Scripts/weapons/FiringHandler.cs
+++ Assets/Scripts/weapons/FiringHandler.cs
@@ -53,9 +53,11 @@ public class FiringHandler : MonoBehaviour {
                     mousePos.z = camDist;
                     Vector3 fireDirection = Camera.main.ScreenToWorldPoint(mousePos) - transform.position;
 
-                    // Send message to fire
-                    networkView.RPC("fireWeapon", RPCMode.Server, Camera.main.ScreenToWorldPoint(mousePos), fireDirection, manager.wepStats.wepType);
-                    // Update fire stats
+                    if (fireDirection.x > 0.2)
+                    {
+                        networkView.RPC("fireWeapon", RPCMode.Server, Camera.main.ScreenToWorldPoint(mousePos), fireDirection, manager.wepStats.wepType);
+                        // Update fire stats
+                    }
                     
                     timer = 0;
 
@@ -81,8 +83,16 @@ public class FiringHandler : MonoBehaviour {
 		// Place Weapon
 		Vector3 startPos = new Vector3(transform.position.x+3, transform.position.y, transform.position.z);
 		//Transform bullet = (Transform)Network.Instantiate(weaponHandler.wepPrefab, startPos, transform.rotation,200);
-       
-		Transform bullet = (Transform)Network.Instantiate(manager.wepStats.wepPrefab, startPos, transform.rotation,200);
+        Transform arm = transform.Find("rightArm");
+        float angle = arm.transform.rotation.z * 100;
+        if (angle < 0) angle = 360 + angle;
+        angle = Mathf.Floor(Mathf.Abs(360 - angle)) * Mathf.PI / 180f;
+        float valX = Mathf.Cos(angle) + transform.position.x + 2.2f;
+        if (angle > 4.71) valX = valX - 1 / Mathf.Cos(angle);
+        float valY = Mathf.Sin(angle) * 3.2f + 1.8f;
+        Vector3 startP = new Vector3(Mathf.Abs(valX), valY, arm.transform.position.z);
+        Debug.Log("Bullet position " + valX + ", " + valY + " Angle " + " , " + Mathf.Cos(angle));
+		Transform bullet = (Transform)Network.Instantiate(manager.wepStats.wepPrefab, startP, transform.rotation,200);
         bullet.name = bullet.name + universeN();
         networkView.RPC("fireAnimation", RPCMode.All, universeN());
 		Physics.IgnoreCollision(bullet.collider, transform.collider);
