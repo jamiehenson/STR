bf990b71eb2f8c5926bb046c41c42c093ef0772f
Jamie Henson <jhenson47@gmail.com>
2013-05-05 21:12:35 +0100

    Music, tinting and lecturers
    Uploaded Sion's music to a file structure, made an Audiosource on OnlineClient and attached a script to randomise music played. Similar deal with camera tinting - used to characterise enemy waves. Also 100% more Simon Hollis implemented.

---
+++ Assets/Scripts/hud/Tint.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using System.Collections;
+
+public class Tint : MonoBehaviour
+{
+	void Start ()
+	{
+		SetRandomColour();
+	}
+
+	public void SetColour(Color colour)
+	{
+		RenderSettings.fog = true;
+		RenderSettings.fogDensity = Random.Range(0.0005f,0.0015f);
+		RenderSettings.fogColor = colour;
+		RenderSettings.ambientLight = colour;
+	}
+
+	public void SetRandomColour()
+	{
+		Color[] colours = {Color.red, Color.blue, Color.green, Color.magenta, Color.yellow, Color.cyan};
+		Color chosenOne = colours[Random.Range(0,colours.Length)];
+		RenderSettings.fog = true;
+		RenderSettings.fogDensity = Random.Range(0.001f,0.002f);
+		RenderSettings.fogColor = chosenOne;
+		RenderSettings.ambientLight = chosenOne;
+	}
+}
