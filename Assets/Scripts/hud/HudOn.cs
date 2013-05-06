082b16979eb17f2f5157c63e2bd32ff24bbebd59
Rob Briggs <superbriggs@googlemail.com>
2013-05-06 21:37:48 +0100

    Vortex countdown goes when you leave the vortex

--- Assets/Scripts/hud/HudOn.cs
+++ Assets/Scripts/hud/HudOn.cs
@@ -488,15 +488,12 @@ public class HudOn : MonoBehaviour {
 	}
 
 	public void leftVortex() {
-		print("Left Vortex");
+		print("In leftVortex");
+		StopCoroutine("VortexCountdown");
 
 		Vortex.labelIsSet = false;
-		
-		if (inVortexCountdown) {
-			//StopCoroutine("VortexCountdown");
-			//StopCoroutine ("Vortex.playerShrink");
-			inVortexCountdown = false;
-		}
+		inVortexCountdown = false;
+		showCountdown = false;
 	}
 
 	public void setManager(PlayerManager m) {
