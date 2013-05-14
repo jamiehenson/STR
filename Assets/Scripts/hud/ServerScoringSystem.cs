a15c14a18687e17e768d2dbda308aad1c3253c8e
Matthew Livingston <matt.l.livingston@gmail.com>
2013-05-14 22:54:24 +0100

    Level times back to normal!

--- Assets/Scripts/hud/ServerScoringSystem.cs
+++ Assets/Scripts/hud/ServerScoringSystem.cs
@@ -6,9 +6,9 @@ public class ServerScoringSystem : MonoBehaviour {
     public string[] playerNames;
     public bool initialized;
 
-    private float levelTime = 10; // in seconds
+    private float levelTime = 30; // in seconds
     private int pauseDelay = 2; // in seconds
-    private int stagesBeforeBoss = 1;
+    private int stagesBeforeBoss = 5;
     private LevelManager[] levelManagers;
     private BossLevelManager bossLevelManager;
 
